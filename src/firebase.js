634f97e0fdcf942fa3a2aacfdcf3380d4065b7e1
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-10 20:10:25 +0530

    feat: added Firestore integration for contact form with basic validation (email, phone)

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-// src/firebase.js
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyABE2csgXRzlkNWVOpa35_ftKr5EYz_hss",
     authDomain: "bbshop-bfc30.firebaseapp.com",
-    projectId: "bbshop-bfc30",                 // ✅ This must match Firebase console
-    storageBucket: "bbshop-bfc30.appspot.com", // ✅ NOT .firebasestorage.app ❌
+    projectId: "bbshop-bfc30",                
+    storageBucket: "bbshop-bfc30.appspot.com",  
     messagingSenderId: "926190778508",
     appId: "1:926190778508:web:970cd69f01143eb8b123a9"
 };
 
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // Firestore instance export
+export const db = getFirestore(app);  
