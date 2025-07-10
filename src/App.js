616ca6262d643e4d49ef5869101144eddd1a469c
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-10 18:49:38 +0530

    minore changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Componets/navbar';
 import Blog from './Componets/Blog';
 import Service from './Componets/navSvc'
